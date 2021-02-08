35aaa0dd86b21f0728a05b93767981364eaa1342
Raffi Manissalian <raffi.manissalian@theoplayer.com>
2021-02-08 15:13:09 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TheramaF3
---
+++ TheramaF3.xcframework/ios-arm64/TheramaF3.framework/Headers/TheramaF3.h
@@ -0,0 +1,19 @@
+//
+//  TheramaF3.h
+//  TheramaF3
+//
+//  Created by RAFFI on 08/02/2021.
+//  Copyright © 2021 RAFFI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TheramaF3.
+FOUNDATION_EXPORT double TheramaF3VersionNumber;
+
+//! Project version string for TheramaF3.
+FOUNDATION_EXPORT const unsigned char TheramaF3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheramaF3/PublicHeader.h>
+
+
